c7acaab7318becdf419b022b68a41dff1516de0e
jeremytouch <kenghtt@gmail.com>
2018-11-05 01:24:38 -0600

    created connection and db in mysql working

---
+++ src/main/java/com/jbom/demo/hello/Application.java
@@ -0,0 +1,23 @@
+package com.jbom.demo.hello;
+
+/**
+ * Project: Jbom
+ * Package: com.jbom.demo.hello
+ * <p>
+ * User: jeremytouch
+ * Date: 11/4/18
+ * Time: 6:44 PM
+ * <p>
+ * Created with IntelliJ IDEA
+ * To change this template use File | Settings | File Templates.
+ */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
